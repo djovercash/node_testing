193d7815fe2ae8e15ab0b720d43508ed467684c8
github username <github email address>
2018-06-05 18:41:43 -0400

    Added describe to clean up tests for legibility

--- utils/server.test.js
+++ utils/server.test.js
@@ -3,27 +3,33 @@ const expect = require('expect')
 
 var app = require('../server/server').app
 
-it('should return error', (done) => {
-  request(app)
-    .get('/')
-    .expect(404)
-    .expect((res) => {
-      expect(res.body).toInclude({
-        error: "Page Not Found"
-      })
+describe('Server', () => {
+  describe('GET /', () => {
+    it('should return error', (done) => {
+      request(app)
+        .get('/')
+        .expect(404)
+        .expect((res) => {
+          expect(res.body).toInclude({
+            error: "Page Not Found"
+          })
+        })
+        .end(done)
     })
-    .end(done)
-})
+  })
 
-it('should include user with first name Drew and age 30', (done) => {
-  request(app)
-    .get('/users')
-    .expect(200)
-    .expect((res) => {
-      expect(res.body).toInclude({
-        name: "Drew",
-        age: 30
-      })
+  describe('GET /users', () => {
+    it('should include user with first name Drew and age 30', (done) => {
+      request(app)
+        .get('/users')
+        .expect(200)
+        .expect((res) => {
+          expect(res.body).toInclude({
+            name: "Drew",
+            age: 30
+          })
+        })
+        .end(done)
     })
-    .end(done)
+  })
 })
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -2,63 +2,68 @@
 const utils = require('./utils')
 const expect = require('expect')
 
-it('should add two numbers', () => {
-  let res = utils.add(33, 11)
 
-  expect(res).toBe(44).toBeA('number')
+describe('Utils', () => {
 
+  describe('#add', () => {
+    it('should add two numbers', () => {
+      let res = utils.add(33, 11)
+      expect(res).toBe(44).toBeA('number')
+      // if (res !== 44) {
+      //   throw new Error(`Expected 44, but got ${res}`)
+      // }
+    })
 
-  // if (res !== 44) {
-  //   throw new Error(`Expected 44, but got ${res}`)
-  // }
-})
-
-it('should add two numbers with async principals', (done) => {
-  utils.asyncAdd(4, 3, (sum) => {
-    expect(sum).toBe(7).toBeA('number')
-    done()
+    it('should add two numbers with async principals', (done) => {
+      utils.asyncAdd(4, 3, (sum) => {
+        expect(sum).toBe(7).toBeA('number')
+        done()
+      })
+    })
   })
-})
 
-it('should square a number', () => {
-  let res = utils.square(20)
+  describe('#square', () => {
+    it('should square a number', () => {
+      let res = utils.square(20)
+      expect(res).toBe(400).toBeA('number')
+      // if (res !== 400) {
+      //   throw new Error(`Expected 400, but got ${res}`)
+      // }
+    })
 
-  expect(res).toBe(400).toBeA('number')
-
-  // if (res !== 400) {
-  //   throw new Error(`Expected 400, but got ${res}`)
-  // }
-})
-
-it('should square a number with async principals', (done) => {
-  utils.asyncSquare(20, (square) => {
-    expect(square).toBe(400).toBeA('number')
-    done()
+    it('should square a number with async principals', (done) => {
+      utils.asyncSquare(20, (square) => {
+        expect(square).toBe(400).toBeA('number')
+        done()
+      })
+    })
   })
-})
 
-it('should expect some values', () => {
-  // expect(12).toNotBe(11)
-  // expect({name: "Drew"}).toEqual({name: "Drew"})
-  // expect([2, 3, 4]).toExclude(1)
-  // expect([2, 3, 4]).toInclude(3)
-  // expect({
-  //   name: "Drew",
-  //   age: 30,
-  //   location: "New York"
-  // }).toInclude({
-  //   age: 30
-  // })
-  // expect({
-  //   name: "Drew",
-  //   age: 30,
-  //   location: "New York"
-  // }).toExclude({
-  //   age: 20
-  // })
-})
+  describe('#randomTests', () => {
+    it('should expect some values', () => {
+      // expect(12).toNotBe(11)
+      // expect({name: "Drew"}).toEqual({name: "Drew"})
+      // expect([2, 3, 4]).toExclude(1)
+      // expect([2, 3, 4]).toInclude(3)
+      // expect({
+      //   name: "Drew",
+      //   age: 30,
+      //   location: "New York"
+      // }).toInclude({
+      //   age: 30
+      // })
+      // expect({
+      //   name: "Drew",
+      //   age: 30,
+      //   location: "New York"
+      // }).toExclude({
+      //   age: 20
+      // })
+    })
 
-it('should verify first and last names are set', () => {
-  let user = utils.setName({location: "New York", age: 30}, "Drew Overcash")
-  expect(user).toInclude({firstName: "Drew", lastName: "Overcash"})
+    it('should verify first and last names are set', () => {
+      let user = utils.setName({location: "New York", age: 30}, "Drew Overcash")
+      expect(user).toInclude({firstName: "Drew", lastName: "Overcash"})
+    })
+  })
 })
