50d4d1c21145d8790acfb8cc438e9eb52b334e3d
github username <github email address>
2018-06-05 17:55:29 -0400

    Added object testing with expect

--- utils/utils.js
+++ utils/utils.js
@@ -1,3 +1,10 @@
 module.exports.add = (a, b) => a + b
 
 module.exports.square = (a) => a * a
+
+module.exports.setName = (user, fullName) => {
+  let nameArr = fullName.split(" ")
+  user.firstName = nameArr[0]
+  user.lastName = nameArr[1]
+  return user
+}
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,18 +1,50 @@
 //BDD Behavior Diven Development
 const utils = require('./utils')
+const expect = require('expect')
 
 it('should add two numbers', () => {
   let res = utils.add(33, 11)
 
-  if (res !== 44) {
-    throw new Error(`Expected 44, but got ${res}`)
-  }
+  expect(res).toBe(44).toBeA('number')
+
+
+  // if (res !== 44) {
+  //   throw new Error(`Expected 44, but got ${res}`)
+  // }
 })
 
 it('should square a number', () => {
   let res = utils.square(20)
 
-  if (res !== 400) {
-    throw new Error(`Expected 400, but got ${res}`)
-  }
+  expect(res).toBe(400).toBeA('number')
+
+  // if (res !== 400) {
+  //   throw new Error(`Expected 400, but got ${res}`)
+  // }
+})
+
+it('should expect some values', () => {
+  // expect(12).toNotBe(11)
+  // expect({name: "Drew"}).toEqual({name: "Drew"})
+  // expect([2, 3, 4]).toExclude(1)
+  // expect([2, 3, 4]).toInclude(3)
+  // expect({
+  //   name: "Drew",
+  //   age: 30,
+  //   location: "New York"
+  // }).toInclude({
+  //   age: 30
+  // })
+  // expect({
+  //   name: "Drew",
+  //   age: 30,
+  //   location: "New York"
+  // }).toExclude({
+  //   age: 20
+  // })
+})
+
+it('should verify first and last names are set', () => {
+  let user = utils.setName({firstName: '', lastName: '', location: "New York", age: 30}, "Drew Overcash")
+  expect(user).toInclude({firstName: "Drew", lastName: "Overcash"})
 })
